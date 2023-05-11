e804070c36db97c703980adf582383680306446b
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2023-05-11 08:21:21 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,24 @@
+let products = [
+  {
+    "img": "img/apple.jpeg",
+    "name": "Apple",
+    "price": "1.25"
+  },
+  {
+    "img": "img/banana.jpeg",
+    "name": "Banana",
+    "price": "1.05"
+  },
+  {
+    "img": "img/stawberry.jpeg",
+    "name": "Strawberry",
+    "price": "1.55"
+  },
+  {
+    "img": "img/watermelon.jpeg",
+    "name": "Watermelon",
+    "price": "1.99"
+  }
+];
+
+export default products;
